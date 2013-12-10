ad510/plausible-deniability

commit a4bfc4a7c480ae442a6b089fa332ac8db5bbaf5b

move several methods into SegmentUnit

Assets/Scripts/SegmentUnit.cs
@@ -13,15 +13,207 @@ using System.Text;
 /// </summary>
 /// <remarks>this is a struct instead of a class because it compares by value</remarks>
 public struct SegmentUnit {
-	public Segment segment;
-	public Unit unit;
+	public readonly Sim g;
+	public readonly Segment segment;
+	public readonly Unit unit;
 	
 	public SegmentUnit(Segment segmentVal, Unit unitVal) {
 		segment = segmentVal;
 		unit = unitVal;
+		g = segment.g;
 	}
 	
 	public SegmentUnit(SegmentUnit segmentUnit) {
 		this = segmentUnit;
+	}
+
+	/// <summary>
+	/// removes unit from segment if doing so wouldn't affect anything that another player saw, returns whether successful
+	/// </summary>
+	public bool delete() {
+		if (!segment.units.Contains (unit.id)) return true; // if this segment already doesn't contain this unit, return true
+		List<SegmentUnit> ancestors = new List<SegmentUnit>();
+		Dictionary<Segment, List<int>> removed = new Dictionary<Segment, List<int>>();
+		long timeEarliestChild = long.MaxValue;
+		int i;
+		ancestors.Add (this);
+		// find all ancestor segments to start removal from
+		for (i = 0; i < ancestors.Count; i++) {
+			if (ancestors[i].prev ().Any ()) {
+				// if this ancestor has a sibling segment that we're not currently planning to remove unit from,
+				// don't remove unit from previous segments shared by both
+				bool hasSibling = false;
+				foreach (Segment seg in ancestors[i].segment.branches) {
+					if (seg.units.Contains (unit.id) && !ancestors.Contains (new SegmentUnit(seg, unit))
+						&& (seg.path.timeSimPast == long.MaxValue || ancestors[i].segment.path.timeSimPast != long.MaxValue)) {
+						hasSibling = true;
+						break;
+					}
+				}
+				if (!hasSibling) {
+					// indicate to remove unit from previous segments
+					ancestors.AddRange (ancestors[i].prev ());
+					ancestors.RemoveAt(i);
+					i--;
+				}
+			}
+			else if (ancestors[i].prev ().Any ()) {
+				// unit has a parent but we're deleting its first segment, so may need to check resources starting at this time
+				timeEarliestChild = Math.Min (timeEarliestChild, ancestors[i].segment.timeStart);
+			}
+			else {
+				// reached a segment with no previous segment whatsoever, so return false (we assume other players know the scenario's starting state)
+				return false;
+			}
+		}
+		// remove unit recursively, starting at the ancestor segments we found
+		for (i = 0; i < ancestors.Count; i++) {
+			if (!ancestors[i].deleteAfter (ref removed, ref timeEarliestChild)) break;
+		}
+		// if a removeUnitAfter() call failed or removing unit might have led to player having negative resources,
+		// add units back to segments they were removed from
+		if (i < ancestors.Count || (timeEarliestChild != long.MaxValue && segment.path.player.checkNegRsc (timeEarliestChild, false) >= 0)) {
+			foreach (KeyValuePair<Segment, List<int>> item in removed) {
+				item.Key.units.AddRange (item.Value);
+			}
+			return false;
+		}
+		// remove paths that no longer contain units from visibility tiles
+		foreach (Segment seg in removed.Keys) {
+			if (seg.id == seg.path.segments.Count - 1 && seg.units.Count == 0 && seg.path.tileX != Sim.OffMap) {
+				g.events.add(new TileMoveEvt(g.timeSim, seg.path.id, Sim.OffMap, 0));
+			}
+		}
+		return true;
+	}
+	
+	private bool deleteAfter(ref Dictionary<Segment, List<int>> removed, ref long timeEarliestChild) {
+		if (segment.units.Contains (unit.id)) {
+			if (!segment.unseen && segment.timeStart < g.timeSim) return false;
+			// only remove units from next segments if this is their only previous segment
+			if (segment.nextOnPath () == null || new SegmentUnit(segment.nextOnPath (), unit).prev ().Count () == 1) {
+				// remove unit from next segments
+				foreach (SegmentUnit segmentUnit in next ()) {
+					if (!segmentUnit.deleteAfter (ref removed, ref timeEarliestChild)) return false;
+				}
+				// remove child units that only this unit could have made
+				foreach (SegmentUnit child in children ().ToArray ()) {
+					// TODO: if has alternate non-live parent, do we need to recursively make children non-live?
+					if (child.parents ().Count () == 1) {
+						if (!child.deleteAfter (ref removed, ref timeEarliestChild)) return false;
+						timeEarliestChild = Math.Min (timeEarliestChild, child.segment.timeStart);
+					}
+				}
+			}
+			// remove unit from this segment
+			segment.units.Remove (unit.id);
+			if (!removed.ContainsKey (segment)) removed.Add (segment, new List<int>());
+			removed[segment].Add (unit.id);
+		}
+		return true;
+	}
+	
+	public bool unseenAfter() {
+		if (!segment.unseen) return false;
+		foreach (SegmentUnit segmentUnit in next ()) {
+			if (!segmentUnit.unseenAfter ()) return false;
+		}
+		foreach (SegmentUnit child in children ()) {
+			if (!child.unseenAfter ()) return false;
+		}
+		return true;
+	}
+
+	/// <summary>
+	/// returns resource amount gained by this unit and its children (subtracting cost to make children)
+	/// from this segment's start time to specified time
+	/// </summary>
+	/// <param name="max">
+	/// since different paths can have collected different resource amounts,
+	/// determines whether to use paths that collected least or most resources in calculation
+	/// </param>
+	// TODO: this currently double-counts child paths/units if paths merge, fix this before enabling stacking
+	public long rscCollected(long time, int rscType, bool max, bool includeNonLiveChildren) {
+		// if this segment wasn't active yet, unit can't have collected anything
+		if (time < segment.timeStart) return 0;
+		// if next segment wasn't active yet, return resources collected from timeStart to time
+		if (segment.nextOnPath () == null || time < segment.nextOnPath ().timeStart) {
+			return unit.type.rscCollectRate[rscType] * (time - segment.timeStart);
+		}
+		long ret = 0;
+		bool foundNextSeg = false;
+		// add resources gained in next segment that collected either least or most resources (depending on max parameter)
+		foreach (SegmentUnit segmentUnit in next ()) {
+			if (includeNonLiveChildren || segmentUnit.segment.path.timeSimPast == long.MaxValue) {
+				long segCollected = segmentUnit.rscCollected (time, rscType, max, includeNonLiveChildren);
+				if (!foundNextSeg || (max ^ (segCollected < ret))) {
+					ret = segCollected;
+					foundNextSeg = true;
+				}
+			}
+		}
+		// add resources gained by children
+		foreach (SegmentUnit child in children ()) {
+			ret += child.rscCollected (time, rscType, max, includeNonLiveChildren);
+			// subtract cost to make child unit
+			ret -= child.unit.type.rscCost[rscType];
+		}
+		// add resources collected on this segment
+		ret += unit.type.rscCollectRate[rscType] * (segment.nextOnPath ().timeStart - segment.timeStart);
+		return ret;
+	}
+	
+	/// <summary>
+	/// returns whether this unit is in the same path before specified time,
+	/// so if it makes a child unit, it's unambiguous who is the parent
+	/// </summary>
+	public bool canBeUnambiguousParent(long time) {
+		return segment.timeStart < time || (segment != null && segment.units.Contains (unit.id));
+	}
+	
+	/// <summary>
+	/// iterates over all segment/unit pairs that could have made this unit in this segment
+	/// </summary>
+	public IEnumerable<SegmentUnit> parents() {
+		if (!prev ().Any ()) {
+			foreach (Segment seg in segment.prev ()) {
+				foreach (SegmentUnit segmentUnit in seg.segmentUnits ()) {
+					if (segmentUnit.unit.type.canMake[unit.type.id]) {
+						yield return segmentUnit;
+					}
+				}
+			}
+		}
+	}
+	
+	/// <summary>
+	/// iterates over all segment/unit pairs that this unit in this segment could have made
+	/// </summary>
+	public IEnumerable<SegmentUnit> children() {
+		foreach (Segment seg in segment.next ()) {
+			foreach (SegmentUnit segmentUnit in seg.segmentUnits ()) {
+				if (unit.type.canMake[segmentUnit.unit.type.id] && !segmentUnit.prev ().Any ()) {
+					yield return segmentUnit;
+				}
+			}
+		}
+	}
+	
+	/// <summary>
+	/// iterates over all segments containing this unit that merge onto the beginning of this segment
+	/// </summary>
+	public IEnumerable<SegmentUnit> prev() {
+		foreach (Segment seg in segment.prev()) {
+			if (seg.units.Contains (unit.id)) yield return new SegmentUnit(seg, unit);
+		}
+	}
+	
+	/// <summary>
+	/// iterates over all segments containing this unit that branch off from the end of this segment
+	/// </summary>
+	public IEnumerable<SegmentUnit> next() {
+		foreach (Segment seg in segment.next()) {
+			if (seg.units.Contains (unit.id)) yield return new SegmentUnit(seg, unit);
+		}
 	}
 }

Assets/Scripts/Path.cs
@@ -164,10 +164,11 @@ public class Path {
 		foreach (int unit in units) {
 			if (segment.units.Contains (unit)) {
 				// unit in path would be child path
+				SegmentUnit segmentUnit = new SegmentUnit(segment, g.units[unit]);
 				// check parent made before (not at same time as) child, so it's unambiguous who is the parent
-				if (!canBeUnambiguousParent (time, segment, unit)) return false;
+				if (!segmentUnit.canBeUnambiguousParent (time)) return false;
 				// check parent unit won't be seen later
-				if (!segment.unseenAfter (g.units[unit])) return false;
+				if (!segmentUnit.unseenAfter ()) return false;
 			}
 			else {
 				if (!canMakeUnitType (time, g.units[unit].type)) return false;
@@ -188,22 +189,14 @@ public class Path {
 	public bool canMakeUnitType(long time, UnitType type) {
 		Segment segment = activeSegment (time);
 		if (segment != null) {
-			foreach (int unit in segment.units) {
-				if (g.units[unit].type.canMake[type.id] && canBeUnambiguousParent (time, segment, unit)
-					&& (time >= g.timeSim || segment.unseenAfter (g.units[unit]))) {
+			foreach (SegmentUnit segmentUnit in segment.segmentUnits ()) {
+				if (segmentUnit.unit.type.canMake[type.id] && segmentUnit.canBeUnambiguousParent (time)
+					&& (time >= g.timeSim || segmentUnit.unseenAfter ())) {
 					return true;
 				}
 			}
 		}
 		return false;
-	}
-	
-	/// <summary>
-	/// returns whether specified unit is in path before specified time,
-	/// so if it makes a child unit, it's unambiguous who is the parent
-	/// </summary>
-	private bool canBeUnambiguousParent(long time, Segment segment, int unit) {
-		return segment.timeStart < time || (segment != null && segment.units.Contains (unit));
 	}
 
 	/// <summary>
@@ -246,7 +239,7 @@ public class Path {
 			// new path will be moved, so try to remove units that will move from this path
 			Segment segment = activeSegment (time);
 			foreach (int unit2 in units) {
-				segment.removeUnit (g.units[unit2]);
+				new SegmentUnit(segment, g.units[unit2]).delete ();
 			}
 		}
 		path2.moveTo (time, pos);
@@ -278,8 +271,8 @@ public class Path {
 		if (time < g.timeSim) {
 			Segment segment = activeSegment (time);
 			if (segment == null) return false;
-			foreach (int unit in segment.units) {
-				if (!segment.unseenAfter (g.units[unit])) return false;
+			foreach (SegmentUnit segmentUnit in segment.segmentUnits ()) {
+				if (!segmentUnit.unseenAfter ()) return false;
 			}
 		}
 		return true;

Assets/Scripts/Player.cs
@@ -53,9 +53,9 @@ public class Player {
 		long ret = startRsc[rscType];
 		for (int i = 0; i < g.nRootPaths; i++) {
 			if (this == g.paths[i].player) {
-				foreach (int unit in g.paths[i].segments[0].units) {
+				foreach (SegmentUnit segmentUnit in g.paths[i].segments[0].segmentUnits ()) {
 					// TODO: this will double-count units that are in multiple paths at beginning of scenario
-					ret += g.paths[i].segments[0].rscCollected(time, g.units[unit], rscType, max, includeNonLiveChildren);
+					ret += segmentUnit.rscCollected(time, rscType, max, includeNonLiveChildren);
 				}
 			}
 		}

Assets/Scripts/Segment.cs
@@ -41,190 +41,7 @@ public class Segment {
 	/// </summary>
 	public void removeAllUnits() {
 		while (units.Count > 0) {
-			if (!removeUnit(g.units[units.Last ()])) throw new SystemException("failed to remove a unit from segment");
-		}
-	}
-
-	/// <summary>
-	/// removes specified unit if doing so wouldn't affect anything that another player saw, returns whether successful
-	/// </summary>
-	public bool removeUnit(Unit unit) {
-		if (!units.Contains (unit.id)) return true; // if this segment already doesn't contain specified unit, return true
-		List<Segment> ancestors = new List<Segment>();
-		Dictionary<Segment, List<int>> removed = new Dictionary<Segment, List<int>>();
-		long timeEarliestChild = long.MaxValue;
-		int i;
-		ancestors.Add (this);
-		// find all ancestor segments to start removal from
-		for (i = 0; i < ancestors.Count; i++) {
-			if (ancestors[i].prev (unit).Any ()) {
-				// if this ancestor has a sibling segment that we're not currently planning to remove unit from,
-				// don't remove unit from previous segments shared by both
-				bool hasSibling = false;
-				foreach (Segment segment in ancestors[i].branches) {
-					if (segment.units.Contains (unit.id) && !ancestors.Contains (segment)
-						&& (segment.path.timeSimPast == long.MaxValue || ancestors[i].path.timeSimPast != long.MaxValue)) {
-						hasSibling = true;
-						break;
-					}
-				}
-				if (!hasSibling) {
-					// indicate to remove unit from previous segments
-					ancestors.AddRange (ancestors[i].prev (unit));
-					ancestors.RemoveAt(i);
-					i--;
-				}
-			}
-			else if (ancestors[i].prev ().Any ()) {
-				// unit has a parent but we're deleting its first segment, so may need to check resources starting at this time
-				timeEarliestChild = Math.Min (timeEarliestChild, ancestors[i].timeStart);
-			}
-			else {
-				// reached a segment with no previous segment whatsoever, so return false (we assume other players know the scenario's starting state)
-				return false;
-			}
-		}
-		// remove unit recursively, starting at the ancestor segments we found
-		for (i = 0; i < ancestors.Count; i++) {
-			if (!ancestors[i].removeUnitAfter (unit, ref removed, ref timeEarliestChild)) break;
-		}
-		// if a removeUnitAfter() call failed or removing unit might have led to player having negative resources,
-		// add units back to segments they were removed from
-		if (i < ancestors.Count || (timeEarliestChild != long.MaxValue && path.player.checkNegRsc (timeEarliestChild, false) >= 0)) {
-			foreach (KeyValuePair<Segment, List<int>> item in removed) {
-				item.Key.units.AddRange (item.Value);
-			}
-			return false;
-		}
-		// remove paths that no longer contain units from visibility tiles
-		foreach (Segment segment in removed.Keys) {
-			if (segment.id == segment.path.segments.Count - 1 && segment.units.Count == 0 && segment.path.tileX != Sim.OffMap) {
-				g.events.add(new TileMoveEvt(g.timeSim, segment.path.id, Sim.OffMap, 0));
-			}
-		}
-		return true;
-	}
-	
-	private bool removeUnitAfter(Unit unit, ref Dictionary<Segment, List<int>> removed, ref long timeEarliestChild) {
-		if (units.Contains (unit.id)) {
-			if (!unseen && timeStart < g.timeSim) return false;
-			// only remove units from next segments if this is their only previous segment
-			if (nextOnPath () == null || nextOnPath ().prev (unit).Count () == 1) {
-				// remove unit from next segments
-				foreach (Segment segment in next (unit)) {
-					if (!segment.removeUnitAfter (unit, ref removed, ref timeEarliestChild)) return false;
-				}
-				// remove child units that only this unit could have made
-				foreach (SegmentUnit child in children (unit).ToArray ()) {
-					// TODO: if has alternate non-live parent, do we need to recursively make children non-live?
-					if (child.segment.parents (child.unit).Count () == 1) {
-						if (!child.segment.removeUnitAfter (child.unit, ref removed, ref timeEarliestChild)) return false;
-						timeEarliestChild = Math.Min (timeEarliestChild, child.segment.timeStart);
-					}
-				}
-			}
-			// remove unit from this segment
-			units.Remove (unit.id);
-			if (!removed.ContainsKey (this)) removed.Add (this, new List<int>());
-			removed[this].Add (unit.id);
-		}
-		return true;
-	}
-	
-	public bool unseenAfter(Unit unit) {
-		if (!units.Contains (unit.id)) throw new ArgumentException("segment does not contain specified unit");
-		if (!unseen) return false;
-		foreach (Segment segment in next (unit)) {
-			if (!segment.unseenAfter (unit)) return false;
-		}
-		foreach (SegmentUnit child in children (unit)) {
-			if (!child.segment.unseenAfter (child.unit)) return false;
-		}
-		return true;
-	}
-
-	/// <summary>
-	/// returns resource amount gained by specified unit and its children (subtracting cost to make children)
-	/// from this segment's start time to specified time
-	/// </summary>
-	/// <param name="max">
-	/// since different paths can have collected different resource amounts,
-	/// determines whether to use paths that collected least or most resources in calculation
-	/// </param>
-	// TODO: this currently double-counts child paths/units if paths merge, fix this before enabling stacking
-	public long rscCollected(long time, Unit unit, int rscType, bool max, bool includeNonLiveChildren) {
-		// if this segment wasn't active yet, unit can't have collected anything
-		if (time < timeStart) return 0;
-		// if next segment wasn't active yet, return resources collected from timeStart to time
-		if (nextOnPath () == null || time < nextOnPath ().timeStart) {
-			return unit.type.rscCollectRate[rscType] * (time - timeStart);
-		}
-		long ret = 0;
-		bool foundNextSeg = false;
-		// add resources gained in next segment that collected either least or most resources (depending on max parameter)
-		foreach (Segment segment in next (unit)) {
-			if (includeNonLiveChildren || segment.path.timeSimPast == long.MaxValue) {
-				long segCollected = segment.rscCollected (time, unit, rscType, max, includeNonLiveChildren);
-				if (!foundNextSeg || (max ^ (segCollected < ret))) {
-					ret = segCollected;
-					foundNextSeg = true;
-				}
-			}
-		}
-		// add resources gained by children
-		foreach (SegmentUnit child in children (unit)) {
-			ret += child.segment.rscCollected (time, child.unit, rscType, max, includeNonLiveChildren);
-			// subtract cost to make child unit
-			ret -= child.unit.type.rscCost[rscType];
-		}
-		// add resources collected on this segment
-		ret += unit.type.rscCollectRate[rscType] * (nextOnPath ().timeStart - timeStart);
-		return ret;
-	}
-	
-	/// <summary>
-	/// iterates over all segment/unit pairs that could have made specified unit in this segment
-	/// </summary>
-	public IEnumerable<SegmentUnit> parents(Unit unit) {
-		if (!prev (unit).Any ()) {
-			foreach (Segment segment in prev ()) {
-				foreach (int unit2 in segment.units) {
-					if (g.units[unit2].type.canMake[unit.type.id]) {
-						yield return new SegmentUnit(segment, g.units[unit2]);
-					}
-				}
-			}
-		}
-	}
-	
-	/// <summary>
-	/// iterates over all segment/unit pairs that specified unit in this segment could have made
-	/// </summary>
-	public IEnumerable<SegmentUnit> children(Unit unit) {
-		foreach (Segment segment in next ()) {
-			foreach (int unit2 in segment.units) {
-				if (unit.type.canMake[g.units[unit2].type.id] && !segment.prev (g.units[unit2]).Any ()) {
-					yield return new SegmentUnit(segment, g.units[unit2]);
-				}
-			}
-		}
-	}
-	
-	/// <summary>
-	/// iterates over all segments containing the specified unit that merge onto the beginning of this segment
-	/// </summary>
-	public IEnumerable<Segment> prev(Unit unit) {
-		foreach (Segment segment in prev()) {
-			if (segment.units.Contains (unit.id)) yield return segment;
-		}
-	}
-	
-	/// <summary>
-	/// iterates over all segments containing the specified unit that branch off from the end of this segment
-	/// </summary>
-	public IEnumerable<Segment> next(Unit unit) {
-		foreach (Segment segment in next()) {
-			if (segment.units.Contains (unit.id)) yield return segment;
+			if (!new SegmentUnit(this, g.units[units.Last ()]).delete ()) throw new SystemException("failed to remove a unit from segment");
 		}
 	}
 	
@@ -262,5 +79,11 @@ public class Segment {
 	public Segment nextOnPath() {
 		if (id == path.segments.Count - 1) return null;
 		return path.segments[id + 1];
+	}
+	
+	public IEnumerable<SegmentUnit> segmentUnits() {
+		foreach (int unit in units) {
+			yield return new SegmentUnit(this, g.units[unit]);
+		}
 	}
 }

Assets/Scripts/Sim.cs
@@ -120,18 +120,18 @@ public class Sim {
 		List<SegmentUnit> prev = new List<SegmentUnit>();
 		bool success = true;
 		for (int i = 0; i < ancestors.Count; i++) {
-			foreach (Segment segment in ancestors[i].segment.prev (ancestors[i].unit)) {
-				ancestors.Add (new SegmentUnit(segment, ancestors[i].unit));
-				if (ancestors[i].segment.path.timeSimPast == long.MaxValue || segment.path.timeSimPast != long.MaxValue) {
-					prev.Add (new SegmentUnit(segment, ancestors[i].unit));
+			foreach (SegmentUnit segmentUnit in ancestors[i].prev ()) {
+				ancestors.Add (segmentUnit);
+				if (ancestors[i].segment.path.timeSimPast == long.MaxValue || segmentUnit.segment.path.timeSimPast != long.MaxValue) {
+					prev.Add (segmentUnit);
 				}
 			}
-			ancestors.AddRange (ancestors[i].segment.parents (ancestors[i].unit));
+			ancestors.AddRange (ancestors[i].parents ());
 		}
 		foreach (SegmentUnit ancestor in prev) {
-			foreach (Segment segment in ancestor.segment.next (ancestor.unit)) {
-				if (!ancestors.Contains (new SegmentUnit(segment, ancestor.unit))) {
-					success &= segment.removeUnit (ancestor.unit);
+			foreach (SegmentUnit segmentUnit in ancestor.next ()) {
+				if (!ancestors.Contains (segmentUnit)) {
+					success &= segmentUnit.delete ();
 				}
 			}
 		}

Assets/Scripts/SimEvt/CmdEvt/DeletePathCmdEvt.cs
@@ -26,7 +26,7 @@ public class DeletePathCmdEvt : CmdEvt {
 		Dictionary<int, List<int>> exPaths = existingPaths (g);
 		foreach (KeyValuePair<int, List<int>> path in exPaths) {
 			foreach (int unit in path.Value) {
-				g.paths[path.Key].activeSegment (timeCmd).removeUnit (g.units[unit]);
+				new SegmentUnit(g.paths[path.Key].activeSegment (timeCmd), g.units[unit]).delete ();
 			}
 		}
 	}

commit d0048d1006f284e7e4b054fc72806b626f8c4ea0

fix SegmentUnit.delete() regression introduced in a4bfc4a

Assets/Scripts/SegmentUnit.cs
@@ -57,7 +57,7 @@ public struct SegmentUnit {
 					i--;
 				}
 			}
-			else if (ancestors[i].prev ().Any ()) {
+			else if (ancestors[i].segment.prev ().Any ()) {
 				// unit has a parent but we're deleting its first segment, so may need to check resources starting at this time
 				timeEarliestChild = Math.Min (timeEarliestChild, ancestors[i].segment.timeStart);
 			}