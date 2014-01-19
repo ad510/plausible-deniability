152408fddfd97a4ba0a3312b95fc27c443f1636f
Andrew Downing <ad510@users.sourceforge.net>
2014-01-18 19:46:10 -0800

    use foreach to iterate over tiles in PlayerVisRemoveEvt

--- Assets/Scripts/SimEvt/PlayerVisRemoveEvt.cs
+++ Assets/Scripts/SimEvt/PlayerVisRemoveEvt.cs
@@ -39,11 +39,11 @@ public class PlayerVisRemoveEvt : SimEvt {
 			}
 		}
 		// add events to remove visibility from surrounding tiles
-		for (int i = 0; i < tiles.Count; i++) {
-			if (tiles[i].x != Sim.OffMap) {
-				for (int tX = Math.Max(0, (int)tiles[i].x - 1); tX <= Math.Min(g.tileLen() - 1, (int)tiles[i].x + 1); tX++) {
-					for (int tY = Math.Max(0, (int)tiles[i].y - 1); tY <= Math.Min(g.tileLen() - 1, (int)tiles[i].y + 1); tY++) {
-						if ((tX != tiles[i].x || tY != tiles[i].y) && g.tiles[tX, tY].playerVisLatest(g.players[player])) {
+		foreach (FP.Vector tile in tiles) {
+			if (tile.x != Sim.OffMap) {
+				for (int tX = Math.Max(0, (int)tile.x - 1); tX <= Math.Min(g.tileLen() - 1, (int)tile.x + 1); tX++) {
+					for (int tY = Math.Max(0, (int)tile.y - 1); tY <= Math.Min(g.tileLen() - 1, (int)tile.y + 1); tY++) {
+						if ((tX != tile.x || tY != tile.y) && g.tiles[tX, tY].playerVisLatest(g.players[player])) {
 							// ISSUE #29: lose visibility in a circle instead of a square
 							g.tiles[tX, tY].playerVisRemove(g.players[player], time + (1 << FP.Precision) / g.maxSpeed);
 						}
@@ -53,14 +53,14 @@ public class PlayerVisRemoveEvt : SimEvt {
 		}
 		if (Sim.EnableNonLivePaths) {
 			// check if a tile stopped being exclusive to this player, or became exclusive to another player
-			int iPrev = -1;
-			for (int i = 0; i < tiles.Count; i++) {
-				if (tiles[i].x != Sim.OffMap) {
-					int tXMax = Math.Min(g.tileLen() - 1, (int)tiles[i].x + g.tileVisRadius());
-					int tYMax = Math.Min(g.tileLen() - 1, (int)tiles[i].y + g.tileVisRadius());
-					for (int tX = Math.Max(0, (int)tiles[i].x - g.tileVisRadius()); tX <= tXMax; tX++) {
-						for (int tY = Math.Max(0, (int)tiles[i].y - g.tileVisRadius()); tY <= tYMax; tY++) {
-							if (g.inVis(tX - tiles[i].x, tY - tiles[i].y) && (iPrev == -1 || !g.inVis(tX - tiles[iPrev].x, tY - tiles[iPrev].y))) {
+			FP.Vector prevTile = new FP.Vector(Sim.OffMap, Sim.OffMap);
+			foreach (FP.Vector tile in tiles) {
+				if (tile.x != Sim.OffMap) {
+					int tXMax = Math.Min(g.tileLen() - 1, (int)tile.x + g.tileVisRadius());
+					int tYMax = Math.Min(g.tileLen() - 1, (int)tile.y + g.tileVisRadius());
+					for (int tX = Math.Max(0, (int)tile.x - g.tileVisRadius()); tX <= tXMax; tX++) {
+						for (int tY = Math.Max(0, (int)tile.y - g.tileVisRadius()); tY <= tYMax; tY++) {
+							if (g.inVis(tX - tile.x, tY - tile.y) && (prevTile.x == Sim.OffMap || !g.inVis(tX - prevTile.x, tY - prevTile.y))) {
 								foreach (Player player2 in g.players) {
 									if (player2.id == player && g.tiles[tX, tY].exclusiveLatest(player2)) {
 										g.tiles[tX, tY].exclusiveRemove(player2, time);
@@ -72,7 +72,7 @@ public class PlayerVisRemoveEvt : SimEvt {
 							}
 						}
 					}
-					iPrev = i;
+					prevTile = tile;
 				}
 			}
 		}
