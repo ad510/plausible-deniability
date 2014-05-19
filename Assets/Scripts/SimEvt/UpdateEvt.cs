e5c9045ed95ba4c5ff237ee594f1c706ddce4f14
Andrew Downing <ad510@users.sourceforge.net>
2014-05-19 07:15:41 -0700

    premature optimization in loop to find target

--- Assets/Scripts/SimEvt/UpdateEvt.cs
+++ Assets/Scripts/SimEvt/UpdateEvt.cs
@@ -83,7 +83,8 @@ public class UpdateEvt : SimEvt {
 						// done reloading, look for closest target to potentially attack
 						Path target = null;
 						long targetDistSq = unit.type.range * unit.type.range + 1;
-						foreach (Path path2 in g.paths) {
+						foreach (int i in tile.pathVis.Keys) {
+							Path path2 = g.paths[i];
 							Segment segment2 = path2.activeSegment (time);
 							if (path != path2 && segment2 != null && path2.timeSimPast == long.MaxValue && path.player.mayAttack[path2.player.id] && tile.pathVisLatest (path2)) {
 								foreach (Unit unit2 in segment2.units) {
