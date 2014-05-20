b05d7ec54e289a362a7e04e1c929de60fe9ca5ac
Andrew Downing <ad510@users.sourceforge.net>
2014-05-20 15:18:34 -0700

    remove timeHealth todo comment, since it's long term and what it says is pretty obvious

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 Andrew Downing
+// Copyright (c) 2013-2014 Andrew Downing
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -20,7 +20,7 @@ public class Unit {
 	[ProtoMember(3, AsReference = true)] public readonly UnitType type;
 	[ProtoMember(4, AsReference = true)] public readonly Player player;
 	[ProtoMember(5)] public int nTimeHealth;
-	[ProtoMember(6)] public long[] timeHealth; // times at which each health increment is removed (TODO: change how this is stored to allow switching units on a path)
+	[ProtoMember(6)] public long[] timeHealth; // times at which each health increment is removed
 	[ProtoMember(7)] public List<Attack> attacks;
 	
 	/// <summary>
