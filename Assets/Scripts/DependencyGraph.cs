9fb5e936fcf17837c6c5bb0a6de80824f87573eb
Andrew Downing <ad510n1@gmail.com>
2015-08-24 01:46:43 -0700

    comment: DependencyGraph is unused but brings back good memories

--- Assets/Scripts/DependencyGraph.cs
+++ Assets/Scripts/DependencyGraph.cs
@@ -1,3 +1,6 @@
+// This file is never used, but I'm leaving it here because it brings back good memories.
+// Thanks Zach!
+
 // Written in 2013 by Zachary Latta
 // To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
 // You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see https://creativecommons.org/publicdomain/zero/1.0/.
